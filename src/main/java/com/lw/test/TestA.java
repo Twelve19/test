f431ff03994505e7e2cbf7248e91802b88e8e5c8
lw <123@qq.com>
2022-07-21 09:27:02 +0800

    test

---
+++ src/main/java/com/lw/test/TestA.java
@@ -0,0 +1,11 @@
+package com.lw.test;
+
+/**
+ * @author lw
+ * @date 2022/7/21 9:21
+ */
+public class TestA {
+    public static void main(String[] args) {
+        System.out.println("I'm master branches");
+    }
+}
